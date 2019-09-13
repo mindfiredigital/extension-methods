ea0b8d9c9fd9f42e0dfcd7a6765fea9f08b4abd4
Swagat Kumar Swain <ssswagatss@gmail.com>
2019-09-13 21:50:51 +0530

    Implements Generic Extensions

--- Extensions.ConsoleApp/Program.cs
+++ Extensions.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace Extensions.ConsoleApp
@@ -7,23 +9,18 @@ namespace Extensions.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //IEnumerable<DateTime> dateRange = DateTime.Now.GetDateRange(DateTime.Now.AddDays(80));
-            //foreach (var item in dateRange)
-            //{
-            //    Console.WriteLine(item.ToMMDDYY());
-            //}
-            //Console.WriteLine(string.Format("{0:(###) ###-####}", 8005551212));
-            //var data = new string[] { "(123) 556 -7890", "123 556 7890", "(123) 556 - 7890", "11111111111" };
-            //foreach (var s in data)
-            //{
-            //    Console.WriteLine(s.MaskPhoneNumber('#'));
-            //}
-            //Console.WriteLine("ssswagatss@gmail.com".MaskEmail('#'));
+            var nums = Enumerable.Range(0, 18);
+            var split = nums.Split(5);
 
-            foreach (HttpStatusCode c in Enum.GetValues(typeof(HttpStatusCode)))
+            foreach (var item in split)
             {
-                Console.WriteLine("Name:{0} - {1} - {2} - {3}", c, c.ToString(), c.ToString().ToHumanCase(), c.ToHumanCase());
+                foreach (var inner in item)
+                {
+                    Console.WriteLine(inner);
+                }
+                Console.WriteLine("----------------");
             }
+
             Console.ReadKey();
         }
     }
---
+++ Extensions/GenericExtensions.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Extensions
+{
+    public static class GenericExtensions
+    {
+        private static Random random = new Random();
+        /// <summary>
+        /// Itterates over any IEnumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="action">The action.</param>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+                action(item);
+        }
+        /// <summary>
+        /// Randomizes the specified target.
+        /// OrderBy() is nice when you want a consistent & predictable ordering. This method is NOT THAT! Randomize() - Use this extension method when you want a different or random order every time! Useful when ordering a list of things for display to give each a fair chance of landing at the top or bottom on each hit.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target">The target.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> target)
+        {
+            Random r = new Random();
+            return target.OrderBy(x => (r.Next()));
+        }
+        /// <summary>
+        /// Determines whether an IEnumerable is null or Empty
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>
+        ///   <c>true</c>if given collection is null or empty otherwise it returns<c>false</c>.
+        /// </returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source == null || (!source.Any());
+        }
+        /// <summary>
+        /// Determines whether an IEnumerable is not null and Empty
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>
+        ///   <c>false</c> if given collection is null or empty otherwise it returns<c>true</c>.
+        /// </returns>
+        public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source != null && source.Any();
+        }
+
+        /// <summary>
+        /// Convert a IEnumerable<T> to a Collection<T>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable">The enumerable.</param>
+        /// <returns></returns>
+        public static Collection<T> ToCollection<T>(this IEnumerable<T> enumerable)
+        {
+            var collection = new Collection<T>();
+            foreach (T i in enumerable)
+                collection.Add(i);
+            return collection;
+        }
+        /// <summary>
+        /// Converts an Enumerable To the CSV string separated by ","
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="instance">The instance.</param>
+        /// <returns></returns>
+        public static string ToCSV<T>(this IEnumerable<T> instance)
+        {
+            StringBuilder csv;
+            if (instance != null)
+            {
+                csv = new StringBuilder();
+                instance.ForEach(v => csv.AppendFormat("{0},", v));
+                return csv.ToString(0, csv.Length - 1);
+            }
+            return string.Empty;
+        }
+        /// <summary>
+        /// Converts an Enumerable To the CSV string.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="instance">The instance.</param>
+        /// <param name="separator">The separator.</param>
+        /// <returns></returns>
+        public static string ToCSV<T>(this IEnumerable<T> instance, char separator)
+        {
+            StringBuilder csv;
+            if (instance != null)
+            {
+                csv = new StringBuilder();
+                instance.ForEach(value => csv.AppendFormat("{0}{1}", value, separator));
+                return csv.ToString(0, csv.Length - 1);
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Converts an IEnumerable to a HashSet
+        /// </summary>
+        /// <typeparam name="T">The IEnumerable type</typeparam>
+        /// <param name="enumerable">The IEnumerable</param>
+        /// <returns>A new HashSet</returns>
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> enumerable)
+        {
+            HashSet<T> hs = new HashSet<T>();
+            foreach (T item in enumerable)
+                hs.Add(item);
+            return hs;
+        }
+        /// <summary>
+        /// Returns the index of the first occurrence in a sequence by using the default equality comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="list">A sequence in which to locate a value.</param>
+        /// <param name="value">The object to locate in the sequence</param>
+        /// <returns>The zero-based index of the first occurrence of value within the entire sequence, if found; otherwise, –1.</returns>
+        public static int IndexOf<TSource>(this IEnumerable<TSource> list, TSource value) where TSource : IEquatable<TSource>
+        {
+
+            return list.IndexOf<TSource>(value, EqualityComparer<TSource>.Default);
+
+        }
+
+        /// <summary>
+        /// Returns the index of the first occurrence in a sequence by using a specified IEqualityComparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="list">A sequence in which to locate a value.</param>
+        /// <param name="value">The object to locate in the sequence</param>
+        /// <param name="comparer">An equality comparer to compare values.</param>
+        /// <returns>The zero-based index of the first occurrence of value within the entire sequence, if found; otherwise, –1.</returns>
+        public static int IndexOf<TSource>(this IEnumerable<TSource> list, TSource value, IEqualityComparer<TSource> comparer)
+        {
+            int index = 0;
+            foreach (var item in list)
+            {
+                if (comparer.Equals(item, value))
+                {
+                    return index;
+                }
+                index++;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Concatenates a specified separator String between each element of a specified enumeration, yielding a single concatenated string.
+        /// </summary>
+        /// <typeparam name="T">any object</typeparam>
+        /// <param name="list">The enumeration</param>
+        /// <param name="separator">A String</param>
+        /// <returns>A String consisting of the elements of value interspersed with the separator string.</returns>
+        public static string ToString<T>(this IEnumerable<T> list, string separator)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var obj in list)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append(separator);
+                }
+                sb.Append(obj);
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Splits the items into different lists depending upon splitSize
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="splitSize">Size of the split.</param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, int splitSize)
+        {
+            using (IEnumerator<T> enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    yield return InnerSplit(enumerator, splitSize);
+                }
+            }
+
+        }
+        /// <summary>
+        /// Continues processing items in a collection until the end condition is true.
+        /// </summary>
+        /// <typeparam name="T">The type of the collection.</typeparam>
+        /// <param name="collection">The collection to iterate.</param>
+        /// <param name="endCondition">The condition that returns true if iteration should stop.</param>
+        /// <returns>Iterator of sub-list.</returns>
+        public static IEnumerable<T> TakeUntil<T>(this IEnumerable<T> collection, Predicate<T> endCondition)
+        {
+            return collection.TakeWhile(item => !endCondition(item));
+        }
+        
+
+        public static T SelectRandom<T>(this IEnumerable<T> sequence)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!sequence.Any())
+            {
+                throw new ArgumentException("The sequence is empty.");
+            }
+
+            //optimization for ICollection<T>
+            if (sequence is ICollection<T>)
+            {
+                ICollection<T> col = (ICollection<T>)sequence;
+                return col.ElementAt(random.Next(col.Count));
+            }
+
+            int count = 1;
+            T selected = default(T);
+
+            foreach (T element in sequence)
+            {
+                if (random.Next(count++) == 0)
+                {
+                    //Select the current element with 1/count probability
+                    selected = element;
+                }
+            }
+
+            return selected;
+        }
+        #region Private Methods
+        private static IEnumerable<T> InnerSplit<T>(IEnumerator<T> enumerator, int splitSize)
+        {
+            int count = 0;
+            do
+            {
+                count++;
+                yield return enumerator.Current;
+            }
+            while (count % splitSize != 0
+                 && enumerator.MoveNext());
+        }
+        #endregion
+    }
+}
