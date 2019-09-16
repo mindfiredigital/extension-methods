0f24850bc64b8b65142d5a3676c316768db666d5
Swagat Kumar Swain <ssswagatss@gmail.com>
2019-09-16 16:04:04 +0530

    Test cases for ToLocalCurrencyString

--- Extensions.UnitTest/NumberExtensionTest.cs
+++ Extensions.UnitTest/NumberExtensionTest.cs
@@ -9,9 +9,31 @@ namespace Extensions.UnitTest
         [InlineData(23.1234, "0.00", "23.12")]
         [InlineData(23.1234, "0.000000", "23.123400")]
         [InlineData(null, "0.000000", "")]
-        public void ToString_ShouldPass(double? data, string format, string actual)
+        public void ToString_nullable_ShouldPass(double? data, string format, string actual)
         {
             var res = data.ToString(format);
+            Assert.Equal(res, actual);
+        }
+
+        [Theory]
+        [InlineData(23, "0.00", "23.00")]
+        [InlineData(23.1234, "0.00", "23.12")]
+        [InlineData(23.1234, "0.000000", "23.123400")]
+        [InlineData(-23, "0.000000", "-23.000000")]
+        public void ToString_ShouldPass(double data, string format, string actual)
+        {
+            var res = data.ToString(format);
+            Assert.Equal(res, actual);
+        }
+        [Theory]
+        [InlineData(23, "$23.00")]
+        [InlineData(0, "$0.00")]
+        [InlineData(98.2311, "$98.23")]
+        [InlineData(98.2389, "$98.24")]
+        [InlineData(1000.00, "$1,000.00")]
+        public void ToLocalCurrencyString_ShouldPass(double data, string actual)
+        {
+            var res = data.ToLocalCurrencyString();
             Assert.Equal(res, actual);
         }
     }
