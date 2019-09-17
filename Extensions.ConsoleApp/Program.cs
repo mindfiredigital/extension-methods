d019f8ad5019b473b47fa092830add7f4884ddd7
taditdash <taditdash@gmail.com>
2019-09-17 19:30:45 +0530

    Add comments to make the examples clear.

--- Extensions.ConsoleApp/Program.cs
+++ Extensions.ConsoleApp/Program.cs
@@ -8,9 +8,16 @@ namespace Extensions.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var l = new List<int>() { 1, 2, 4, 8, 5, 3, 0 };
+            var l = new List<int>() { 1, 2, 4, 8, 5, 3 };
             l.Sort();
-            l.InsertSorted(0);
+
+            // Sort the list before calling InsertSorted().
+            // The method below inserts 6 at the appropriate position inside the sorted list.
+            l.InsertSorted(6);
+
+            // Extension Method ToString(", ") would make your list nicely formatted with comma separated.
+            Console.WriteLine(l.ToString(", "));
+
             Console.ReadKey();
         }
     }
