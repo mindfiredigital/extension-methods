7300f52bde6d7d4c2de210286bc8f67ebf9ed2a2
Swagat Kumar Swain <ssswagatss@gmail.com>
2021-03-08 22:19:57 +0530

    adds documentation for json extensions

--- Extensions.ConsoleApp/Program.cs
+++ Extensions.ConsoleApp/Program.cs
@@ -9,9 +9,28 @@ namespace Extensions.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Color color = "ffffcc88".ToColor();
-            var bar = color.R;
+            //var obj = new Bar()
+            //{
+            //    FirstName = "Swagat",
+            //    LastName = "Swain",
+            //    Age = 29
+            //};
+            //var jsonString = obj.ToJson();
+            //Console.WriteLine(jsonString);
+
+
+            var jsonString = "{\"FirstName\":\"Swagat\",\"LastName\":\"Swain\",\"Age\":29}";
+            //var bar = jsonString.JsonToObject<Bar>();
+            var foo = jsonString.JsonToDictionary();
+            
             Console.ReadKey();
         }
+    }
+
+    public class Bar
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
     }
 }
